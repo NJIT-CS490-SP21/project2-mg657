21115a8f7274dc0cb90d58a47f0f9053f9011251
mahi <ec2-user@ip-172-31-30-45.ec2.internal>
2021-02-28 00:18:58 +0000

    Added conditional rendering of board and winner

--- src/App.js
+++ src/App.js
@@ -8,46 +8,8 @@ import io from 'socket.io-client';
 const socket = io(); // Connects to socket connection
 
 function App() {
-  const [players, setPlayers] = useState({"PlayerX":"", "PlayerO": "", "Spectators": []}); // State variable, list of players
-  const playerRef = useRef(null); // Reference to <input> element
-  function onClickButton() {
-    if (playerRef != null) {
-      const player = playerRef.current.value;
-      var playerCopy = {...players};
-      if (playerCopy["PlayerX"]!="" && playerCopy["PlayerO"]!=""){
-        playerCopy["Spectators"].push(player);
-      }
-      else if (playerCopy["PlayerX"]==""){
-        playerCopy["PlayerX"] = player;
-      }
-      else if (playerCopy["PlayerX"]!="" && playerCopy["PlayerO"]==""){
-        playerCopy["PlayerO"] = player;
-      }
-      setPlayers(playerCopy);
-      //console.log(playerCopy);
-      socket.emit('login', {players: playerCopy});
-    }
-  }
-  useEffect(() => {
-    socket.on('login', (data) => {
-      console.log('Login event received!');
-      var newPlayer = {...data.players}
-      setPlayers(newPlayer);
-      });
-  }, []);
-  console.log(players);
   return (
     <div>
-      <h1>Login Here</h1>
-      Enter username: <input ref={playerRef} type="text" />
-      <button onClick={onClickButton}>Submit</button>
-      <ul>
-      <li>Player X: {players["PlayerX"]}</li>
-      <li>Player O: {players["PlayerO"]}</li>
-      {players["Spectators"].map((item) => <li>Spectators: {item} </li> )}
-      </ul>
-      
-     
       <Board />
     </div>
   );
--- src/Board.js
+++ src/Board.js
@@ -8,22 +8,80 @@ const socket = io(); // Connects to socket connection
 export function Board(){
     const [board, setBoard] = useState(["","","","","","","","",""]);
     const [ isX, setX ] = useState(true);
+    const [players, setPlayers] = useState({"PlayerX":"", "PlayerO": "", "Spectators": []}); // State variable, list of players
+    const playerRef = useRef(null); // Reference to <input> element
+    const [isLogged, setLog] = useState(false);
+    const [playerX, setPlayerX] = useState(false);
+    const [playerO, setPlayerO] = useState(false);
+    const [spectators, setSpectators] = useState(false);
     function onClickSquare(index){
         let newBoard = [...board];
+        const currUser = playerRef.current.value;
+        if(players["Spectators"].includes(currUser)){
+          return;
+        }
         newBoard[index] = isX ? "X" : "O";
         setBoard(prevBoard =>newBoard);
         setX(!isX);
         socket.emit('board', {board: newBoard[index], index: index, isX: isX});
   }
+ 
+  
   function isDraw(board){
     var winner = "";
     if(isBoardFull(board) && !calculateWinner(board)){
-      console.log("DRAW");
-      winner = "It is a draw";}
+      winner = <p>It is a draw</p>;}
     else{
-      winner = calculateWinner(board);
-      }
+      winner = <p>The winner is <div>{calculateWinner(board)}!</div></p>;}
     return winner;
+  }
+ 
+  function onClickButton() {
+    if (playerRef != null) {
+      setLog(true);
+      const player = playerRef.current.value;
+      var playerCopy = {...players};
+      if (playerCopy["PlayerX"]!="" && playerCopy["PlayerO"]!=""){
+        playerCopy["Spectators"].push(player);
+        setSpectators(true);
+      }
+      else if (playerCopy["PlayerX"]==""){
+        playerCopy["PlayerX"] = player;
+        setPlayerX(true);
+      }
+      else if (playerCopy["PlayerX"]!="" && playerCopy["PlayerO"]==""){
+        playerCopy["PlayerO"] = player;
+        setPlayerO(true);
+      }
+      setPlayers(playerCopy);
+      socket.emit('login', {players: playerCopy});
+    }
+  }
+   function displayPlayers(){
+    if(playerX || playerO || spectators){
+      return (<div><h3>Player X</h3>{players["PlayerX"]}
+      <h3>Player O</h3> {players["PlayerO"]}
+      <h3>Spectators</h3>
+      {players["Spectators"].map((item) => <p>{item} </p> )}
+      {isLogged ? 
+            <div>
+            <div class="board">
+    <Square onClickSquare={onClickSquare} board={board} index={0} />
+    <Square onClickSquare={onClickSquare} board={board} index={1} />
+    <Square onClickSquare={onClickSquare} board={board} index={2} />
+    <Square onClickSquare={onClickSquare} board={board} index={3} />
+    <Square onClickSquare={onClickSquare} board={board} index={4} />
+    <Square onClickSquare={onClickSquare} board={board} index={5} />
+    <Square onClickSquare={onClickSquare} board={board} index={6} />
+    <Square onClickSquare={onClickSquare} board={board} index={7} />
+    <Square onClickSquare={onClickSquare} board={board} index={8} />
+    </div>
+    {isDraw(board)}
+    </div>:null}</div>);
+    }
+    else{
+      return;
+    }
   }
 
   useEffect(() => {
@@ -39,20 +97,18 @@ export function Board(){
       });
       console.log(data);
     });
+    socket.on('login', (data) => {
+      console.log('Login event received!');
+      var newPlayer = {...data.players}
+      setPlayers(newPlayer);
+      });
   }, []);
+  console.log(players);
     return (
     <div>
-    <div class="board">
-    <Square onClickSquare={onClickSquare} board={board} index={0} />
-    <Square onClickSquare={onClickSquare} board={board} index={1} />
-    <Square onClickSquare={onClickSquare} board={board} index={2} />
-    <Square onClickSquare={onClickSquare} board={board} index={3} />
-    <Square onClickSquare={onClickSquare} board={board} index={4} />
-    <Square onClickSquare={onClickSquare} board={board} index={5} />
-    <Square onClickSquare={onClickSquare} board={board} index={6} />
-    <Square onClickSquare={onClickSquare} board={board} index={7} />
-    <Square onClickSquare={onClickSquare} board={board} index={8} />
-    </div>
-    <li>The winner is: {isDraw(board)}</li>
+    <h1>Login </h1>
+    <input ref={playerRef} type="text" placeholder = "Enter Username" />
+    <button onClick={onClickButton}>Submit</button>
+    {displayPlayers()}
     </div>);
 }
